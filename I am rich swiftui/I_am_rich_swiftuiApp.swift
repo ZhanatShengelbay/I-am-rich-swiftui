15bc1a03f6ee6cb0c66e73523bdea6317668eaac
Zhanat <62037412+ZhanatShengelbay@users.noreply.github.com>
2024-07-19 08:56:40 +0200

    Initial Commit

---
+++ I am rich swiftui/I_am_rich_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_rich_swiftuiApp.swift
+//  I am rich swiftui
+//
+//  Created by Zhanat Shengelbayeva on 19/07/24.
+//
+
+import SwiftUI
+
+@main
+struct I_am_rich_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
